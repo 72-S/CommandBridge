c352a824569d423cb5ca1b1b415c7d029020c63c
72-S <consti.bauer@outlook.com>
2024-08-07 15:55:30 +0200

    feat: Add support for parsing block commands in StringParser
    - Added method `parseBlockCommands` to handle parsing block commands
    - Added placeholders for %server%, %world%, %player%, and %uuid% in block commands
    
    This commit enhances the StringParser class to support parsing block commands in addition to player and console commands. The new method `parseBlockCommands` replaces placeholders for %server%, %world%, %player%, and %uuid% with the corresponding values from the BlockCommandSender.

--- Bukkit/src/main/java/org/commandbridge/utilities/StringParser.java
+++ Bukkit/src/main/java/org/commandbridge/utilities/StringParser.java
@@ -1,14 +1,26 @@
 package org.commandbridge.utilities;
 
+import org.bukkit.command.BlockCommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
+
+import java.util.Objects;
 
 public class StringParser {
     public static String parsePlaceholders(String command, Player player) {
         return command.replace("%player%", player.getName())
                 .replace("%uuid%", player.getUniqueId().toString())
-                .replace("%server%", player.getServer().getName());
+                .replace("%server%", player.getServer().getName())
+                .replace("%world%", player.getWorld().getName());
     }
     public static String parseConsoleCommands(String command, ConsoleCommandSender consoleCommandSender) {
-        return command.replace("%server%", consoleCommandSender.getServer().getName());}
+        return command.replace("%server%", consoleCommandSender.getServer().getName());
+    }
+
+    public static String parseBlockCommands(String command, BlockCommandSender blockCommandSender) {
+        return command.replace("%server%", blockCommandSender.getServer().getName())
+                .replace("%world%", blockCommandSender.getBlock().getWorld().getName())
+                .replace("%player%", Objects.requireNonNull(blockCommandSender.getServer().getPlayer(blockCommandSender.getName())).getName())
+                .replace("%uuid%", Objects.requireNonNull(blockCommandSender.getServer().getPlayer(blockCommandSender.getName())).getUniqueId().toString());
+    }
 }
