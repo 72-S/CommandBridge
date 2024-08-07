867e41c75e8d38d6296f26c11a9164531a3fa2d7
72-S <consti.bauer@outlook.com>
2024-08-07 16:05:01 +0200

    feat: Add support for parsing command arguments in StringParser
    Added support for parsing command arguments in the `StringParser` class by adding a new parameter `String[] args` to the `parsePlaceholders`, `parseConsoleCommands`, and `parseBlockCommands` methods. Also, introduced a new private method `appendArguments` to concatenate the arguments into a single string. This enhancement improves the flexibility and functionality of the command parsing process.

--- Bukkit/src/main/java/org/commandbridge/utilities/StringParser.java
+++ Bukkit/src/main/java/org/commandbridge/utilities/StringParser.java
@@ -3,24 +3,36 @@ package org.commandbridge.utilities;
 import org.bukkit.command.BlockCommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
+import org.commandbridge.command.manager.CommandRegister;
 
 import java.util.Objects;
 
 public class StringParser {
-    public static String parsePlaceholders(String command, Player player) {
+    public static String parsePlaceholders(String command, Player player, String[] args) {
         return command.replace("%player%", player.getName())
                 .replace("%uuid%", player.getUniqueId().toString())
                 .replace("%server%", player.getServer().getName())
-                .replace("%world%", player.getWorld().getName());
+                .replace("%world%", player.getWorld().getName())
+                .replace("%args%", Objects.requireNonNull(appendArguments(args)));
     }
-    public static String parseConsoleCommands(String command, ConsoleCommandSender consoleCommandSender) {
-        return command.replace("%server%", consoleCommandSender.getServer().getName());
+    public static String parseConsoleCommands(String command, ConsoleCommandSender consoleCommandSender, String[] args) {
+        return command.replace("%server%", consoleCommandSender.getServer().getName())
+                .replace("%args%", Objects.requireNonNull(appendArguments(args)));
     }
 
-    public static String parseBlockCommands(String command, BlockCommandSender blockCommandSender) {
+    public static String parseBlockCommands(String command, BlockCommandSender blockCommandSender, String[] args) {
         return command.replace("%server%", blockCommandSender.getServer().getName())
                 .replace("%world%", blockCommandSender.getBlock().getWorld().getName())
                 .replace("%player%", Objects.requireNonNull(blockCommandSender.getServer().getPlayer(blockCommandSender.getName())).getName())
-                .replace("%uuid%", Objects.requireNonNull(blockCommandSender.getServer().getPlayer(blockCommandSender.getName())).getUniqueId().toString());
+                .replace("%uuid%", Objects.requireNonNull(blockCommandSender.getServer().getPlayer(blockCommandSender.getName())).getUniqueId().toString())
+                .replace("%args%", Objects.requireNonNull(appendArguments(args)));
+    }
+
+
+    private static String appendArguments(String[] args) {
+        if (args.length > 0) {
+            return String.join(" ", args);
+        }
+        return null;
     }
 }
