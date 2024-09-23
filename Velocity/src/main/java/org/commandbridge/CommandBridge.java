914bbe634323c290f23ca2a23d46ea2683a23b59
72S <118930716+72-S@users.noreply.github.com>
2024-09-23 20:47:57 +0000

    Bump version to 1.8.1-SNAPSHOT in POM files and CommandBridge class

--- Velocity/src/main/java/org/commandbridge/CommandBridge.java
+++ Velocity/src/main/java/org/commandbridge/CommandBridge.java
@@ -21,7 +21,7 @@ import org.slf4j.Logger;
 import java.util.ArrayList;
 import java.util.List;
 
-@Plugin(id = "commandbridge", name = "CommandBridgeVelocity", version = "1.7.1-SNAPSHOT", description = "A plugin to bridge commands between servers", authors = {"72S_"}, url = "https://modrinth.com/plugin/commandbridge")
+@Plugin(id = "commandbridge", name = "CommandBridgeVelocity", version = "1.8.1-SNAPSHOT", description = "A plugin to bridge commands between servers", authors = {"72S_"}, url = "https://modrinth.com/plugin/commandbridge")
 public class CommandBridge {
 
     private final ProxyServer server;
