aec35d36c17e29175c17e3c063f2349320f614dc
72-S <consti.bauer@outlook.com>
2025-02-10 16:41:05 +0100

    cb is now in release state not in beta anymore

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,7 @@ include("paper", "velocity")
 gradle.extra["pversion"] = "2.1.3"
 
 
-gradle.extra["pluginType"] = "beta"
+gradle.extra["pluginType"] = "release"
 
 gradle.extra["pluginVersions"] = listOf("1.20.5", "1.20.6", "1.21", "1.21.1", "1.21.2", "1.21.3", "1.21.4")
 
